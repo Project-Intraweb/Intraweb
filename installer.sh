ce82c5070ad378a0bf7cb4e2da5087381b94f9ec
Tim N <timnguyen737@gmail.com>
2020-03-07 16:08:36 -0500

    updates

--- installer.sh
+++ installer.sh
@@ -1,112 +1,35 @@
 #!/bin/bash
 
 sudo apt update -y
-sudo apt install -y snapd wget ca-certificates sudo lsb-release python3 python3-setuptools python3-dev python3-pip
+sudo apt install -y snapd wget ca-certificates sudo lsb-release python3 python3-setuptools python3-dev python3-pip dnsmasq hostapd
+sudo systemctl stop dnsmasq
+sudo systemctl stop hostapd
 sudo snap install rocketchat-server
 pip3 install zimply
 sudo mv zimply.py /home/pi/zimply.py
 sudo mv zimply.service /etc/systemd/system/zimply.service
 sudo systemctl enable zimply.service
 sudo systemctl start zimply.service
-
-#sudo mv /home/pi/Intraweb/ports.conf /etc/apache2/ports.conf
-#sudo mv /home/pi/Intraweb/000-default.conf /etc/apache2/sites-enabled/000-default.conf
-#sudo /etc/init.d/apache2 reload
-#!/bin/bash
-
-# NextCloudPi installation script
-#
-# Copyleft 2017 by Ignacio Nunez Hernanz <nacho _a_t_ ownyourbits _d_o_t_ com>
-# GPL licensed (see end of file) * Use at your own risk!
-#
-# Usage: ./install.sh
-#
-# more details at https://ownyourbits.com
-git clone https://github.com/Piflyer/nextcloudpi.git
-cd nextcloudpi
-BRANCH=master
-#DBG=x
-
-set -e$DBG
-
-TMPDIR="$(mktemp -d /tmp/nextcloudpi.XXXXXX || (echo "Failed to create temp dir. Exiting" >&2 ; exit 1) )"
-trap "rm -rf \"${TMPDIR}\" ; exit 0" 0 1 2 3 15
-
-[[ ${EUID} -ne 0 ]] && {
-  printf "Must be run as root. Try 'sudo $0'\n"
-  exit 1
-}
-
-export PATH="/usr/local/sbin:/usr/sbin:/sbin:${PATH}"
-
-# check installed software
-type mysqld  &>/dev/null && echo ">>> WARNING: existing mysqld configuration will be changed <<<"
-
-# get install code
-echo "Getting build code..."
-apt-get update
-apt-get install --no-install-recommends -y wget ca-certificates sudo lsb-release
-
-pushd "$TMPDIR"
-wget -qO- --content-disposition https://github.com/nextcloud/nextcloudpi/archive/"$BRANCH"/latest.tar.gz \
-  | tar -xz \
-  || exit 1
-cd - && cd "$TMPDIR"/nextcloudpi-"$BRANCH"
-
-# install NCP
-echo -e "\nInstalling NextCloudPi..."
-source etc/library.sh
-
-# check distro
-check_distro etc/ncp.cfg || {
-  echo "ERROR: distro not supported:";
-  cat /etc/issue
-  exit 1;
-}
-
-
-mkdir -p /usr/local/etc/ncp-config.d/
-cp etc/ncp-config.d/nc-nextcloud.cfg /usr/local/etc/ncp-config.d/
-cp etc/library.sh /usr/local/etc/
-cp etc/ncp.cfg /usr/local/etc/
-
-install_app    lamp.sh
-install_app    bin/ncp/CONFIG/nc-nextcloud.sh
-run_app_unsafe bin/ncp/CONFIG/nc-nextcloud.sh
-systemctl restart mysqld # TODO this shouldn't be necessary, but somehow it's needed in Debian 9.6. Fixme
-install_app    ncp.sh
-run_app_unsafe bin/ncp/CONFIG/nc-init.sh
-bash /usr/local/bin/ncp-provisioning.sh
-
-popd
-
-IFACE="$( ip r | grep "default via" | awk '{ print $5 }' | head -1 )"
-IP="$( ip a show dev "$IFACE" | grep global | grep -oP '\d{1,3}(.\d{1,3}){3}' | head -1 )"
-
-echo "Done."
-
-
-
-# License
-#
-# This script is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This script is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this script; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place, Suite 330,
-# Boston, MA  02111-1307  USA
-
-popd
+sudo apt-get install apache2 -y
+sudo apt-get install php7.3 php7.3-gd sqlite php7.3-sqlite3 php7.3-curl php7.3-zip php7.3-xml php7.3-mbstring
+sudo service apache2 restart
+curl https://download.nextcloud.com/server/releases/nextcloud-18.0.1.zip
+sudo unzip nextcloud-18.0.1.zip
+sudo mkdir -p /var/www/html/nextcloud/data
+sudo chown -R www-data:www-data /var/www/html/nextcloud/
+sudo chmod 750 /var/www/html/nextcloud/data
+sudo mv dhcpcd.conf /etc/dhcpcd.conf
+sudo service dhcpcd restart
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+sudo mv dnsmasq.conf /etc/dnsmasq.conf
+sudo systemctl start dnsmasq
+sudo mv hostapd.conf /etc/hostapd/hostapd.conf
+sudo sh -c 'echo DAEMON_CONF="/etc/hostapd/hostapd.conf" > /etc/default/hostapd'
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
+sudo systemctl start hostapd
 IFACE="$( ip r | grep "default via" | awk '{ print $5 }' | head -1 )"
 IP="$( ip a show dev "$IFACE" | grep global | grep -oP '\d{1,3}(.\d{1,3}){3}' | head -1 )"
 echo "Done.
-First: Visit http://$IP:880 to configure your nextcloudpi instance. Depending how large your zim file was, it may still take a while for ti to fully index it. Visit http://$IP to see all apps. Have fun!"
+First: Visit http://$IP/nextcloud to configure your Nextcloud instance. Depending how large your zim file was, it may still take a while for ti to fully index it. Visit http://$IP to see all apps. Have fun!"
 exit 0
