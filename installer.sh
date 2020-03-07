7a7f863ecaa2137db085dc393bb128644dc52ef5
Tim N <timnguyen737@gmail.com>
2020-03-07 11:46:54 -0500

    Updates

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo -i
+
 sudo apt update -y
 sudo apt install -y snapd wget ca-certificates sudo lsb-release python3 python3-setuptools python3-dev python3-pip
 sudo snap install rocketchat-server
@@ -8,29 +8,101 @@ sudo mv zimply.py /home/pi/zimply.py
 sudo mv zimply.service /etc/systemd/system/zimply.service
 sudo systemctl enable zimply.service
 sudo systemctl start zimply.service
-mktemp -d /tmp/nextcloudpi && cd /tmp/nextcloudpi
-wget -qO- --content-disposition https://github.com/nextcloud/nextcloudpi/archive/master/latest.tar.gz
-cd nextcloudpi-master
+
+#sudo mv /home/pi/Intraweb/ports.conf /etc/apache2/ports.conf
+#sudo mv /home/pi/Intraweb/000-default.conf /etc/apache2/sites-enabled/000-default.conf
+#sudo /etc/init.d/apache2 reload
+#!/bin/bash
+
+# NextCloudPi installation script
+#
+# Copyleft 2017 by Ignacio Nunez Hernanz <nacho _a_t_ ownyourbits _d_o_t_ com>
+# GPL licensed (see end of file) * Use at your own risk!
+#
+# Usage: ./install.sh
+#
+# more details at https://ownyourbits.com
+
+BRANCH=master
+#DBG=x
+
+set -e$DBG
+
+TMPDIR="$(mktemp -d /tmp/nextcloudpi.XXXXXX || (echo "Failed to create temp dir. Exiting" >&2 ; exit 1) )"
+trap "rm -rf \"${TMPDIR}\" ; exit 0" 0 1 2 3 15
+
+[[ ${EUID} -ne 0 ]] && {
+  printf "Must be run as root. Try 'sudo $0'\n"
+  exit 1
+}
+
+export PATH="/usr/local/sbin:/usr/sbin:/sbin:${PATH}"
+
+# check installed software
+type mysqld  &>/dev/null && echo ">>> WARNING: existing mysqld configuration will be changed <<<"
+
+# get install code
+echo "Getting build code..."
+apt-get update
+apt-get install --no-install-recommends -y wget ca-certificates sudo lsb-release
+
+pushd "$TMPDIR"
+wget -qO- --content-disposition https://github.com/nextcloud/nextcloudpi/archive/"$BRANCH"/latest.tar.gz \
+  | tar -xz \
+  || exit 1
+cd - && cd "$TMPDIR"/nextcloudpi-"$BRANCH"
+
+# install NCP
+echo -e "\nInstalling NextCloudPi..."
 source etc/library.sh
+
+# check distro
 check_distro etc/ncp.cfg || {
   echo "ERROR: distro not supported:";
   cat /etc/issue
   exit 1;
 }
+
+
 mkdir -p /usr/local/etc/ncp-config.d/
 cp etc/ncp-config.d/nc-nextcloud.cfg /usr/local/etc/ncp-config.d/
 cp etc/library.sh /usr/local/etc/
 cp etc/ncp.cfg /usr/local/etc/
+
 install_app    lamp.sh
 install_app    bin/ncp/CONFIG/nc-nextcloud.sh
 run_app_unsafe bin/ncp/CONFIG/nc-nextcloud.sh
-systemctl restart mysqld
+systemctl restart mysqld # TODO this shouldn't be necessary, but somehow it's needed in Debian 9.6. Fixme
 install_app    ncp.sh
 run_app_unsafe bin/ncp/CONFIG/nc-init.sh
 bash /usr/local/bin/ncp-provisioning.sh
-sudo mv /home/pi/Intraweb/ports.conf /etc/apache2/ports.conf
-sudo mv /home/pi/Intraweb/000-default.conf /etc/apache2/sites-enabled/000-default.conf
-sudo /etc/init.d/apache2 reload
+
+popd
+
+IFACE="$( ip r | grep "default via" | awk '{ print $5 }' | head -1 )"
+IP="$( ip a show dev "$IFACE" | grep global | grep -oP '\d{1,3}(.\d{1,3}){3}' | head -1 )"
+
+echo "Done."
+
+
+
+# License
+#
+# This script is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This script is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this script; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+# Boston, MA  02111-1307  USA
+
 popd
 IFACE="$( ip r | grep "default via" | awk '{ print $5 }' | head -1 )"
 IP="$( ip a show dev "$IFACE" | grep global | grep -oP '\d{1,3}(.\d{1,3}){3}' | head -1 )"
