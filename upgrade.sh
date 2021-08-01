ef4b748ddb668c4e2a070f1cf3618c1f82c41763
Tim Nguyen <timnguyen737@gmail.com>
2021-08-01 09:21:08 -0400

    Upgrade script
    To be updated with every change

---
+++ upgrade.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo rm -r *.html /var/www/html
+sudo rm -r *.map /var/www/html
+sudo rm -r *.css /var/www/html
+sudo rm -r *.js /var/www/html
+sudo rm -r *.jpeg /var/www/html
+sudo rm -r *.png /var/www/html
+sudo rm -r *.json /var/www/html
+sudo mv *.html /var/www/html
+sudo mv backdrops /var/www/html
+sudo mv logo /var/www/html
