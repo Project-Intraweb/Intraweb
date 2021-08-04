f2736ae3b95b62dcf1e5ad6d5634b5544357a2b2
Sid220 <74916637+Sid220@users.noreply.github.com>
2021-08-04 10:26:57 -0400

    Delete phet.sh

--- phet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget https://project-intraweb.github.io/phet.zip
-mkdir phet
-unzip phet.zip -d phet
-mv ./phet /var/www/html/
-jq '.phet=true' prefs.json > tmp.$$.json && mv tmp.$$.json prefs.json
