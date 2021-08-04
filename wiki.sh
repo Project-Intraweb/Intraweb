da8a2b5c79d98d60c58faf0513bc80d091195000
Sid220 <74916637+Sid220@users.noreply.github.com>
2021-08-04 10:27:40 -0400

    Delete wiki.sh

--- wiki.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pip3 install zimply
-sudo mv zimply.py /home/pi/wiki.py
-sudo mv zimply.service /etc/systemd/system/zimply.service
-sudo systemctl enable zimply.service
-sudo systemctl start zimply.service
-jq '.wiki=true' prefs.json > tmp.$$.json && mv tmp.$$.json prefs.json
